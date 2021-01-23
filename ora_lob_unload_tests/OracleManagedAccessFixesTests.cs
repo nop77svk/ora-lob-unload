d76f8c89b86e470c5ed223ad9046574b44740824
pierre@home <nop77svk@gmail.com>
2021-01-23 01:39:50 +0100

    + unit test suite stub

---
+++ ora_lob_unload_tests/OracleManagedAccessFixesTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OraLobUnload;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OraLobUnload.Tests
+{
+    [TestClass()]
+    public class OracleManagedAccessFixesTests
+    {
+        [TestMethod()]
+        public void CorrectlyCopyToTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
