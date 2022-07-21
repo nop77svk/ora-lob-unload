b1e37fb4cfcb48ed8a0518f5c844223f3ec4d6d6
pierre@audrey <nop77svk@gmail.com>
2022-07-22 00:36:38 +0200

    🧹 ora_lob_unload project folder renamed to match its project name

---
+++ OraLobUnload/CommandLineOptions.cs
@@ -0,0 +1,171 @@
+﻿namespace NoP77svk.OraLobUnload;
+
+using System;
+using System.Text;
+using CommandLine;
+using NoP77svk.OraLobUnload.ConnectStringParser;
+using NoP77svk.OraLobUnload.DataReaders;
+
+internal class CommandLineOptions
+{
+    private readonly OracleConnectStringParsed _dbLogonParsed = new OracleConnectStringParsed();
+
+    [Option('u', "logon", Required = true, HelpText = "\nFull database connection string in form\n<username>[/<password>]@<database>[ as sysdba| as sysoper]")]
+    public string? DbLogonFull
+    {
+        get => _dbLogonParsed.FullConnectString;
+        set { _dbLogonParsed.FullConnectString = value ?? ""; }
+    }
+
+    [Option('i', "input", Required = false, HelpText = "Input script file\nIf not provided, stdin is used")]
+    public string? InputFile { get; set; }
+
+    [Option('t', "input-content", Group = "Input Content", Required = false, HelpText = "\nInput script file content type\n(One of) query, out-ref-cursor, tables, implicit-cursors")]
+    public string? InputFileContentDesc
+    {
+        get => InputFileContentType switch
+        {
+            InputContentType.Select => "query",
+            InputContentType.OutRefCursor => "out-ref-cursor",
+            InputContentType.Tables => "tables",
+            InputContentType.ImplicitCursors => "implicit-cursors",
+            _ => throw new ArgumentOutOfRangeException($"Don''t know how to interpret file content type {InputFileContentType}")
+        };
+
+        set
+        {
+            InputFileContentType = value?.Trim()?.ToLower()?.Replace("-", "")?.Replace("_", "") switch
+            {
+                "select" or "query" => InputContentType.Select,
+                "outrefcursor" or "outcursor" or "refcursor" or "cursor" => InputContentType.OutRefCursor,
+                "table" or "tables" => InputContentType.Tables,
+                "implicitcursors" or "implicitcursor" or "implicit" => InputContentType.ImplicitCursors,
+                _ => throw new ArgumentOutOfRangeException($"Don''t know how to interpret file content type {value}")
+            };
+        }
+    }
+
+    [Option('q', "input-content-query", Group = "Input Content", Required = false, HelpText = "\nShorthand for --input-content=query")]
+    public bool InputFilecontentSelect
+    {
+        get => InputFileContentType == InputContentType.Select;
+        set
+        {
+            InputFileContentType = InputContentType.Select;
+        }
+    }
+
+    [Option('c', "input-content-cursor", Group = "Input Content", Required = false, HelpText = "\nShorthand for --input-content=out-ref-cursor")]
+    public bool InputFileContentOutCursor
+    {
+        get => InputFileContentType == InputContentType.OutRefCursor;
+        set
+        {
+            InputFileContentType = InputContentType.OutRefCursor;
+        }
+    }
+
+    [Option("input-content-tables", Group = "Input Content", Required = false, HelpText = "\nShorthand for --input-content=tables")]
+    public bool InputScriptTypeTables
+    {
+        get => InputFileContentType == InputContentType.Tables;
+        set
+        {
+            InputFileContentType = InputContentType.Tables;
+        }
+    }
+
+    [Option('m', "input-content-implicit-cursors", Group = "Input Content", Required = false, HelpText = "\nShorthand for --input-content=implicit-cursors")]
+    public bool InputScriptTypeImplicit
+    {
+        get => InputFileContentType == InputContentType.ImplicitCursors;
+        set
+        {
+            InputFileContentType = InputContentType.ImplicitCursors;
+        }
+    }
+
+    [Option('o', "output", Required = false, HelpText = "Output folder")]
+    public string? OutputFolder { get; set; }
+
+    [Option('x', "output-file-extension", Required = false, HelpText = "Output file extension to be appended to the file name if not already appended by DB")]
+    public string? OutputFileExtension { get; set; }
+
+    [Option("clob-output-charset", Required = false, Default = "utf-8", HelpText = "\nConvert CLOBs automatically to target charset")]
+    public string? OutputEncodingId { get; set; }
+
+    [Option("file-name-column-ix", Required = false, Default = 1, HelpText = "\nFile name is in column #x of the data set(s) being read")]
+    public int FileNameColumnIndex { get; set; }
+
+    [Option("lob-column-ix", Required = false, Default = 2, HelpText = "\nLOB contents are in column #x of the data set(s) being read")]
+    public int LobColumnIndex { get; set; }
+
+    [Option("lob-init-fetch-size", Required = false, Default = "0", HelpText = "\n(Internal) Initial LOB fetch size\nUse \"K\" or \"M\" suffixes to denote 1KB or 1MB sizes respectively")]
+    internal string? LobInitFetchSize { get; set; }
+
+    internal OracleConnectStringParsed DbLogon
+    {
+        get => _dbLogonParsed;
+    }
+
+    /*
+    [Option("db", Required = false, HelpText = "Database (either as a TNS alias or an EzConnect string) to connect to")]
+    internal string? DbService
+    {
+        get => _dbLogonParsed.DbService;
+        set { _dbLogonParsed.DbService = value ?? ""; }
+    }
+
+
+    [Option("user", Required = false, HelpText = "Database user name to connect to")]
+    internal string? DbUser
+    {
+        get => _dbLogonParsed.User;
+        set { _dbLogonParsed.User = value ?? ""; }
+    }
+
+    [Option("pass", Required = false, HelpText = "The connecting database user's password")]
+    internal string? DbPassword
+    {
+        get => _dbLogonParsed.Password;
+        set { _dbLogonParsed.Password = value ?? ""; }
+    }
+
+    internal OracleUserConnectRole DbUserRole
+    {
+        get => _dbLogonParsed.SpecialRole;
+        set { _dbLogonParsed.SpecialRole = value; }
+    }
+    */
+
+    internal Encoding OutputEncoding => OutputEncodingId switch
+    {
+        null or "" => new UTF8Encoding(false, false),
+        _ => Encoding.GetEncoding(OutputEncodingId)
+    };
+
+    internal int LobInitFetchSizeB
+    {
+        get
+        {
+            if (LobInitFetchSize is null or "")
+            {
+                return 65536;
+            }
+            else
+            {
+                string lobFetchWoUnit = LobInitFetchSize[0..^1];
+                if (LobInitFetchSize.EndsWith("K", StringComparison.OrdinalIgnoreCase))
+                    return Convert.ToInt32(lobFetchWoUnit) * 1024;
+                else if (LobInitFetchSize.EndsWith("M", StringComparison.OrdinalIgnoreCase))
+                    return Convert.ToInt32(lobFetchWoUnit) * 1024 * 1024;
+                else if (LobInitFetchSize.EndsWith("G", StringComparison.OrdinalIgnoreCase))
+                    return Convert.ToInt32(lobFetchWoUnit) * 1024 * 1024 * 1024;
+                else
+                    throw new ArgumentOutOfRangeException(nameof(LobInitFetchSize), $"Unrecognized unit of LOB fetch size \"{LobInitFetchSize}\"");
+            }
+        }
+    }
+
+    internal InputContentType InputFileContentType { get; set; }
+}
---
+++ OraLobUnload/Program.cs
@@ -0,0 +1,181 @@
+﻿namespace NoP77svk.OraLobUnload;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CommandLine;
+using NoP77svk.OraLobUnload.DataReaders;
+using NoP77svk.OraLobUnload.StreamColumnProcessors;
+using NoP77svk.OraLobUnload.Utilities;
+using Oracle.ManagedDataAccess.Client;
+
+internal static class Program
+{
+    private static readonly HashSet<string> _foldersCreated = new ();
+
+    internal static int Main(string[] args)
+    {
+        return Parser.Default.ParseArguments<CommandLineOptions>(args)
+            .MapResult<CommandLineOptions, int>(
+                options => MainWithOptions(options),
+                _ => 255
+            );
+    }
+
+    internal static int MainWithOptions(CommandLineOptions options)
+    {
+        Console.Error.WriteLine("Oracle LOB Unloader");
+        Console.Error.WriteLine($"by Peter Hraško a.k.a NoP77svk");
+        Console.Error.WriteLine($"https://github.com/NoP77svk/ora-lob-unload");
+        Console.Error.WriteLine();
+
+        try
+        {
+            ValidateCommandLineArguments(options);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine($"ERROR: {e.Message}");
+            return 254;
+        }
+
+        using StreamReader inputSqlScriptReader = OpenInputSqlScript(options.InputFile);
+
+        Console.Error.WriteLine($"Connecting to {options.DbLogon.DisplayableConnectString}");
+        using var dbConnection = OracleConnectionFactory(options.DbLogon.DbService, options.DbLogon.User, options.DbLogon.Password);
+        dbConnection.Open();
+
+        Console.Error.WriteLine($"Using {InputScriptFactory.GetInputSqlReturnTypeDesc(options.InputFileContentType)} as an input against the database");
+        var dbCommandFactory = new InputScriptFactory(dbConnection, options.LobInitFetchSizeB);
+        using IDataMultiReader dataMultiReader = dbCommandFactory.CreateMultiReader(options.InputFileContentType, inputSqlScriptReader);
+
+        if (options.OutputFolder is not null and not "")
+            Console.Error.WriteLine($"Output folder: {options.OutputFolder}");
+        else
+            Console.Error.WriteLine("Output folder: (current)");
+
+        Console.Error.WriteLine($"Output CLOBs encoding: {options.OutputEncoding.HeaderName}");
+
+        foreach (OracleDataReader dbReader in dataMultiReader.CreateDataReaders())
+        {
+            using (dbReader)
+            {
+                int leastDatasetColumnCountNeeded = Math.Max(options.FileNameColumnIndex, options.LobColumnIndex);
+                if (dbReader.FieldCount < leastDatasetColumnCountNeeded)
+                    throw new InvalidDataException($"Dataset field count is {dbReader.FieldCount}, should be at least {leastDatasetColumnCountNeeded}");
+
+                string fileNameColumnTypeName = dbReader.GetFieldType(options.FileNameColumnIndex - 1).Name;
+                if (fileNameColumnTypeName != "String")
+                    throw new InvalidDataException($"Supposed file name column #{options.FileNameColumnIndex} is of type \"{fileNameColumnTypeName}\", but \"string\" expected");
+
+                using IStreamColumnProcessor processor = StreamColumnProcessorFactory(
+                    dbReader.GetProviderSpecificFieldType(options.LobColumnIndex - 1),
+                    $"# {options.LobColumnIndex - 1} ({dbReader.GetName(options.LobColumnIndex - 1)})",
+                    options.OutputEncoding
+                );
+                SaveDataFromReader(
+                    dbReader,
+                    options.FileNameColumnIndex - 1,
+                    options.LobColumnIndex - 1,
+                    processor,
+                    options.OutputFileExtension,
+                    options.OutputFolder
+                );
+            }
+        }
+
+        Console.Error.WriteLine("DONE");
+        return 0;
+    }
+
+    internal static void CreateFilePath(string? filePath)
+    {
+        if (filePath is not null and not "")
+        {
+            if (!_foldersCreated.Contains(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+                _foldersCreated.Add(filePath);
+            }
+        }
+    }
+
+    internal static StreamReader OpenInputSqlScript(string? inputSqlScriptFile)
+    {
+        return inputSqlScriptFile switch
+        {
+            "" or null => new StreamReader(Console.OpenStandardInput()),
+            _ => File.OpenText(inputSqlScriptFile)
+        };
+    }
+
+    internal static OracleConnection OracleConnectionFactory(string? dbService, string? dbUser, string? dbPassword)
+    {
+        if (dbService is null or "")
+            throw new ArgumentNullException(nameof(dbService));
+        if (dbUser is null or "")
+            throw new ArgumentNullException(nameof(dbUser));
+        if (dbPassword is null or "")
+            throw new ArgumentNullException(nameof(dbPassword));
+
+        return new OracleConnection($"Data Source = {dbService}; User Id = {dbUser}; Password = {dbPassword}");
+    }
+
+    internal static void SaveDataFromReader(OracleDataReader dataReader, int fileNameColumnIx, int lobColumnIx, IStreamColumnProcessor processor, string? fileNameExt, string? outputPath)
+    {
+        string cleanedFileNameExt = fileNameExt is not null and not "" ? "." + fileNameExt.Trim('.') : "";
+        while (dataReader.Read())
+        {
+            string fileName = dataReader.GetString(fileNameColumnIx);
+            fileName = Path.Combine(outputPath ?? "", fileName);
+            string fileNameWithExt = cleanedFileNameExt != "" && !fileName.EndsWith(cleanedFileNameExt, StringComparison.OrdinalIgnoreCase)
+                ? fileName + cleanedFileNameExt
+                : fileName;
+
+            CreateFilePath(Path.GetDirectoryName(fileNameWithExt));
+            using Stream outFile = new FileStream(fileNameWithExt, FileMode.Create, FileAccess.Write);
+
+            using Stream lobContents = processor.ReadLob(dataReader, lobColumnIx);
+            Console.Error.Write($"{fileNameWithExt} [{processor.GetFormattedLobLength(lobContents.Length)}] ...");
+            processor.SaveLobToStream(lobContents, outFile);
+            Console.Error.WriteLine(" done");
+        }
+    }
+
+    internal static IStreamColumnProcessor StreamColumnProcessorFactory(Type columnType, string columnDescription, Encoding charColumnOutputEncoding)
+    {
+        return columnType.Name switch
+        {
+            "OracleClob" => new ClobProcessor(charColumnOutputEncoding),
+            "OracleBlob" => new BlobProcessor(),
+            "OracleBFile" => new BFileProcessor(),
+            _ => throw new InvalidDataException($"Supposed LOB column {columnDescription} is of type \"{columnType.Name}\", but CLOB, BLOB or BFILE expected")
+        };
+    }
+
+    internal static void ValidateCommandLineArguments(CommandLineOptions options)
+    {
+        if (options.DbLogon.DbService is null or "")
+            throw new ArgumentNullException(null, "Database service name not supplied");
+        if (options.DbLogon.User is null or "")
+            throw new ArgumentNullException(null, "Connecting database user not supplied");
+
+        if (options.DbLogon.Password is null or "")
+        {
+            Console.Error.Write($"Enter password for {options.DbLogon.DisplayableConnectString}: ");
+            Random charRandomizer = new Random();
+            options.DbLogon.Password = SystemConsoleExt.ReadLineInSecret((x) => Convert.ToChar(charRandomizer.Next(32, 127)), true);
+        }
+
+        if (options.DbLogon.Password is null or "")
+            throw new ArgumentNullException(null, "Connecting database user's password not supplied");
+
+        if (options.FileNameColumnIndex is < 1 or > 1000)
+            throw new ArgumentOutOfRangeException(null, "File name column index must be between 1 and 1000 (inclusive)");
+        if (options.LobColumnIndex is < 1 or > 1000)
+            throw new ArgumentOutOfRangeException(null, "LOB column index must be between 1 and 1000 (inclusive)");
+        if (options.LobColumnIndex == options.FileNameColumnIndex)
+            throw new ArgumentException($"LOB column index {options.LobColumnIndex} cannot be the same as file name column index {options.FileNameColumnIndex}");
+    }
+}
